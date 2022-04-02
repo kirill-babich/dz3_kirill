b58e6fc946705e10cdf02f439e18ac9242c1bd77
Kirill <kirill.babich.2016@gmail.com>
2022-04-02 18:33:49 +0300

    product

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,5 +1,23 @@
 const swiper = new Swiper('.swiper',{
     loop: true,
-    slidesPerView: 5,
-    spaceBetween: 200,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        // when window width is >= 320px
+        520: {
+            slidesPerView: 2,
+          },
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 480px
+        1024: {
+          slidesPerView: 4,
+        },
+        // when window width is >= 640px
+        1300: {
+          slidesPerView: 5,
+        }
+        
+      }
 });
